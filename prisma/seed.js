3c412dc64c2af217f8670c83db8538b0270db6d6
marcusDevGit <marcus_psc@hotmail.com>
2025-05-18 15:38:29 -0300

    fix(seed): converte sintaxe de ES Modules para CommonJS

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,6 +1,6 @@
 // prisma/seed.js
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcrypt';
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
 
 const prisma = new PrismaClient();
 
